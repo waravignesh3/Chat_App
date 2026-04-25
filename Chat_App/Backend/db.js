2239985bcd2784c24780ea143c36e43e81fbf57b
Vigneshwara S <waravignesh3@gmail.com>
2026-04-25 12:46:20 +0530

    fixed

--- Chat_App/Backend/db.js
+++ Chat_App/Backend/db.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || "localhost",
+  host: process.env.DB_HOST || "10.116.74.151",
   user: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD || "",
   database: process.env.DB_NAME || "chatapp",
